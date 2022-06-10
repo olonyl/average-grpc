b7a239ef7737594144d272d49e87a96980aea7a5
Olonyl Landeros <jose87.landeros@gmail.com>
2022-06-10 13:24:15 -0500

    Add project files.

---
+++ client/Program.cs
@@ -0,0 +1,41 @@
+﻿using Average;
+using Grpc.Core;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        const string Server = "127.0.0.1:50023";
+        static async Task Main(string[] args)
+        {
+            Channel channel = new Channel(Server, ChannelCredentials.Insecure);
+            await channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The client connected successfully");
+                }
+            });
+
+            var client = new AverageService.AverageServiceClient(channel);
+            var stream = client.ComputeAverage();
+
+            foreach (var item in Enumerable.Range(1, 4))
+            {
+                var request = new AverageRequest { Number = item };
+                await stream.RequestStream.WriteAsync(request);
+            }
+
+            await stream.RequestStream.CompleteAsync();
+            var response = await stream.ResponseAsync;
+
+            Console.WriteLine($"This is the average {response.Result}");
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,41 @@
+﻿using Average;
+using Grpc.Core;
+using System;
+using System.IO;
+
+namespace server
+{
+    class Program
+    {
+        const int Port = 50023;
+        static void Main(string[] args)
+        {
+            Server server = null;
+
+            try
+            {
+                server = new Server
+                {
+                    Services = { AverageService.BindService(new AverageServiceImpl()) },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine($"The server is listening on Port: {Port}");
+                Console.ReadKey();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error Initializing the server in port {Port}", ex.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
+
+
+        }
+    }
+}
